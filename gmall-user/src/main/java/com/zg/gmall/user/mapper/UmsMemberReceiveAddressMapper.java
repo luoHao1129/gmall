8b8488c1647c1c49e6cccda5be582e81364af14f
luohao <327626141@qq.com>
2019-12-16 12:15:17 +0800

    12.16 提交测试

---
+++ gmall-user/src/main/java/com/zg/gmall/user/mapper/UmsMemberReceiveAddressMapper.java
@@ -0,0 +1,7 @@
+package com.zg.gmall.user.mapper;
+
+import com.zg.gmall.user.bean.UmsMemberReceiveAddress;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UmsMemberReceiveAddressMapper extends Mapper<UmsMemberReceiveAddress> {
+}
